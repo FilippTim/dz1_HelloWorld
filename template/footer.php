4aa22594bb441fb246566028a95bcdcdee94e433
Filipp_Timos <filipptim000@gmail.com>
2023-10-24 23:52:54 +0300

    Немного подвинул футер

--- template/footer.php
+++ template/footer.php
@@ -1,18 +1,18 @@
 <html>
     <body>
-        <p><br><br><br><br><br><br><br><br><br><br><br><br></p>
-            <div align="left" style="margin-left:50px;">Задание для самостоятельной работы: «Hello, World!»:</div>
+            <br><br>
+            <h3 align="left">Задание для самостоятельной работы: «Hello, World!»:</h3>
             <ol>
                 <li><div align="left">   Создать веб-страницу с динамическим контентом. </div></li>
                 <li><div align="left">Загрузить код в удаленный репозиторий.</div></li>
                 <li><div align="left">Залить на хостинг.</div></li>
             </ol>
-            <div align="left" style="margin-left:20px;">   Дано:</div>
-            <div align="left" style="margin-left:20px;">  Header = слева логотип МосПолитеха, по центру название работы.</div>
-            <div align="left" style="margin-left:20px;">  Footer = задание для самостоятельно работы (без описания).</div>
-            <div align="left" style="margin-left:20px;">  Main = любой html-элемент с адекватным динамическим контентом (пример Hello, World).<br></div>
-            <div align="left" style="margin-left:20px;"> <br>Ответ на гугл диск:</div>
-            <div align="left" style="margin-left:20px;">  URL – адрес страницы.</div>
-            <<div align="left" style="margin-left:20px;">  Ссылка на репозиторий</div>
+            <div align="left">   Дано:</div>
+            <div align="left">  Header = слева логотип МосПолитеха, по центру название работы.</div>
+            <div align="left">  Footer = задание для самостоятельно работы (без описания).</div>
+            <div align="left">  Main = любой html-элемент с адекватным динамическим контентом (пример Hello, World).<br></div>
+            <div align="left"> <br>Ответ на гугл диск:</div>
+            <div align="left">  URL – адрес страницы.</div>
+            <<div align="left">  Ссылка на репозиторий</div>
     </body>
 </html>
